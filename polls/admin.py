02c92e5c660aa69bf2415564a04d0a8cce6553b8
Noah Presler <noah@presler,me>
2016-10-20 20:05:24 -0400

    choices in admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Choice)
