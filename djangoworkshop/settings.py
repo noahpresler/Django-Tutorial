001c7425f62f7aa7ca5e9f2368b0b328a8b0ff6b
Max Yeo <mzwyeo@gmail.com>
2016-10-21 13:49:41 -0400

    starter template file and static files

--- djangoworkshop/settings.py
+++ djangoworkshop/settings.py
@@ -120,3 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.getcwd(),'static'),
+
+)
+
+STATIC_ROOT=""
