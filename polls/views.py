e36a813989477bc0a5ea84bf6df92192ef8b59ff
Kristin Yim <kristinyim@Kristins-MacBook-Pro.local>
2016-10-22 00:16:33 -0400

    progress

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
